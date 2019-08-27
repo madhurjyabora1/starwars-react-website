aac12eb05f7ab70747ded6b076d1c985ea547590
MadhurjyaBora <mrbora10@gmail.com>
2019-08-27 17:15:11 +0530

    Initial Commit

---
+++ build/precache-manifest.2cfc82fe156fb0d7982380d5968ac10c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7bb1523d0b6f8e5926eb85e44b06603",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc1c09f5b3802293579a",
+    "url": "/static/css/main.c4455299.chunk.css"
+  },
+  {
+    "revision": "32d890e4ae12e07ac984",
+    "url": "/static/js/2.3f3e57aa.chunk.js"
+  },
+  {
+    "revision": "dc1c09f5b3802293579a",
+    "url": "/static/js/main.7d05f7bc.chunk.js"
+  },
+  {
+    "revision": "18eeb7aba4f87b1a4873",
+    "url": "/static/js/runtime~main.d1d9f46a.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/starwars/precache-manifest.546964e28a3748d478ad8c7ff7832896.js"
+  "/precache-manifest.2cfc82fe156fb0d7982380d5968ac10c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/starwars/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
